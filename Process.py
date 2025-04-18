74d7cadb7975a0f7437eb6088e4ee7e2b62124b3
Hang Bao <baohang873@gmail.com>
2025-04-17 17:35:47 -0700

    added Process.py as model

---
+++ Process.py
@@ -0,0 +1,11 @@
+class Process:
+    def __init__(self, pid, arrival_time, burst_time, priority):
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.remain_time = burst_time
+        self.priority = priority
+        self.start_time = 0
+        self.completion_time = 0
+        self.turn_around_time = 0
+        self.waiting_time = 0
